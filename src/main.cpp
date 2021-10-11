722126c5291422c406ce03653d55d16c4edfb3ea
RogerCashley <71915548+RogerCashley@users.noreply.github.com>
2021-10-11 15:40:55 +0700

    initialzied project structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+/******************************************
+ * Edric, Mikha, Roger
+ * Data Structures and ALgorithm
+ * Mr. Fikri
+ * 
+ * Scientific Calculator with Data Structures
+ ******************************************/
+
 #include <iostream>
 
+#include "../includes/queue.h"
+#include "../includes/linkedlist.h"
+
 int main() {
+    std::cout << "Hello World!" << std::endl;
 
     return 0;
 }
