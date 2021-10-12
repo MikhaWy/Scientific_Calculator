73045652646b663d8b0b1d2ef5b6eac50615a03e
RogerCashley <71915548+RogerCashley@users.noreply.github.com>
2021-10-12 10:14:08 +0700

    power function and decimal value

---
+++ includes/calculator.h
@@ -0,0 +1,13 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    double Evaluate(const char* expression);
+private:
+    int precedence(const char& op) const;
+    double arithmetic(const double& a, const double& b, const char& op) const;
+    double pow(const double& a, const double& n) const;
+};
+
+#endif
---
+++ src/calculator.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+
+#include "../includes/calculator.h"
+#include "../includes/queue.h"
+
+int Calculator::precedence(const char& op) const {
+    if (op == '+' || op == '-')
+        return 1;
+    else if (op == '*' || op == '/')
+        return 2;
+    else if (op == '^')
+        return 3;
+    return 0;
+}
+
+double Calculator::arithmetic(const double& a, const double& b, const char& op) const {
+    switch (op) {
+    case '+': return a + b;
+    case '-': return a - b;
+    case '*': return a * b;
+    case '/': return a / b;
+    case '^': return pow(a, b);
+    default:
+        std::cout << "ERROR: Invalid operator!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+double Calculator::pow(const double& a, const double& n) const {
+    int raise = (int)(n + 0.5 - (n < 0));
+    double temp;
+    if (raise == 0)
+        return 1;
+    temp = pow(a, raise / 2);
+    if (raise % 2 == 0) {
+        return temp * temp;
+    }
+    else {
+        if (raise > 0)
+            return a * temp * temp;
+        else 
+            return (temp * temp) / a;
+    }
+}
+
+double Calculator::Evaluate(const char* expression) {
+    Queue<double> numbers(100); // Stores all the numeric values
+    Queue<char> ops(100);       // Stores all the operators
+    int i;
+
+    for (i = 0; i < strlen(expression); i++) {
+        // Ignore whitespace(s)
+        if (expression[i] == ' ')
+            continue;
+        // Current index is an opening bracket
+        else if (expression[i] == '(') {
+            ops.Enqueue(expression[i]);
+        }
+        // Current index is a number
+        else if (isdigit(expression[i])) {
+            double current_value = 0; // Loop till a non digit is reached
+            int decimal_place = 0;
+            while (i < strlen(expression) && (isdigit(expression[i]) || expression[i] == '.')) {
+                if (expression[i] != '.') {
+                    // - '0' is the same as -49 since ascii value for char '0' is 49
+                    current_value = (current_value * 10.0) + (expression[i] - '0');
+                    i++;
+
+                    if (decimal_place > 0)
+                        decimal_place++;
+                }
+                else {
+                    decimal_place++;
+                    i++;
+                }
+            }
+
+            if (decimal_place != 0)
+                numbers.Enqueue(current_value / pow(10, decimal_place - 1));
+            else
+                numbers.Enqueue(current_value);
+            i--;
+        }
+        // Given string is a negative number -1 {
+        else if (expression[i] == '-' && isdigit(expression[i + 1])) {
+            double current_value = 0;
+
+        }
+        // Solve the entire bracket if a closing bracket is found
+        else if (expression[i] == ')') {
+            while (!ops.IsEmpty() && ops.Rear() != '(') {
+                double number2 = numbers.Pop();
+                double number1 = numbers.Pop();
+                char op = ops.Pop();
+
+                numbers.Enqueue(arithmetic(number1, number2, op));
+            }
+
+            if (!ops.IsEmpty())
+                ops.Pop();
+        }
+        // Current index is an operator
+        else {
+            // Calculate if the latest operator in the queue has a higher precedence than 
+            // the current index
+            while (!ops.IsEmpty() && (precedence(ops.Rear()) >= precedence(expression[i]))) {
+                double number2 = numbers.Pop();
+                double number1 = numbers.Pop();
+                char op = ops.Pop();
+
+                numbers.Enqueue(arithmetic(number1, number2, op));
+            }
+
+            ops.Enqueue(expression[i]);
+        }
+    }
+
+    while (!ops.IsEmpty()) {
+        double number2 = numbers.Pop();
+        double number1 = numbers.Pop();
+        char op = ops.Pop();
+
+        numbers.Enqueue(arithmetic(number1, number2, op));
+    }
+
+    return numbers.Rear();
+}
